e20410c7cbcb949696b89342031c8212ebfcecc3
tan.nguyen <tan.nguyenrd@datalogic.com>
2023-09-19 16:16:49 +0700

    create GUI for input path

--- main.py
+++ main.py
@@ -1,19 +1,8 @@
 import os
 import shutil
 import xml.etree.ElementTree as ET
-
-# Variable
-
-# Required path
-file_load_path = "C:/VL_EVL/vl_test_tool_config/Test_Suite/Function/VL5.load"
-# Folder that used to save data
-test_result_path = "C:/TestData/"
-# Place that save vlc file and itc file
-vlc_itc_path = "C:/VL_EVL/vl_test_tool_config/"
-# Image folder
-image_origin_path = "//dl44h0lb3/IP-TECH/"
-# Robo file path
-robo_path = "C:/TestData/test.robot"
+import tkinter as tk
+from tkinter import ttk, filedialog
 
 # Global variable:
 # Address of sym folder
@@ -23,6 +12,20 @@ test_type_path = ""
 # Address of testcase folder
 TCID_path = ""
 
+file_load_path = ""
+robo_path = ""
+test_result_path = ""
+vlc_itc_path = ""
+image_origin_path = ""
+
+
+def choosefile_insertlabel(filepath, dialogname, label):
+    filepath = filedialog.askopenfilename(title=dialogname)
+    label.config(text=filepath)
+
+def choosefolder_insertlabel(folderpath, dialogname, label):
+    folderpath = filedialog.askdirectory(title=dialogname)
+    label.config(text=folderpath)
 
 # To delete special character
 def reformat_folder_name(folder_name):
@@ -32,70 +35,104 @@ def reformat_folder_name(folder_name):
             temp_name = temp_name + char
     return temp_name
 
+def run(file_load_path, robo_path, test_result_path, vlc_itc_path, image_origin_path):
+    tree = ET.parse(file_load_path)
+    root = tree.getroot()
 
-tree = ET.parse(file_load_path)
-root = tree.getroot()
+    for symbology_content in root:
+        sym_name = symbology_content.text.strip()
+        symbology_path = test_result_path + sym_name
+        os.makedirs(symbology_path)
+        print("Symbology: ", sym_name)
 
-for symbology_content in root:
-    sym_name = symbology_content.text.strip()
-    symbology_path = test_result_path + sym_name
-    os.makedirs(symbology_path)
-    print("Symbology: ", sym_name)
+        for test_type_content in symbology_content:
+            type_name = test_type_content.text.strip()
+            type_name = reformat_folder_name(type_name)
+            test_type_path = symbology_path + "/" + type_name
+            os.makedirs(test_type_path)
+            print("Test type: ", type_name)
 
-    for test_type_content in symbology_content:
-        type_name = test_type_content.text.strip()
-        type_name = reformat_folder_name(type_name)
-        test_type_path = symbology_path + "/" + type_name
-        os.makedirs(test_type_path)
-        print("Test type: ", type_name)
+            for testcase_content in test_type_content:
+                testcase_id = testcase_content.find('ExpectResultID').text
+                TCID_path = test_type_path + "/"+ testcase_id
+                os.makedirs(TCID_path)
+                print("Testcase ID: ", testcase_id)
 
-        for testcase_content in test_type_content:
-            testcase_id = testcase_content.find('ExpectResultID').text
-            TCID_path = test_type_path + "/"+ testcase_id
-            os.makedirs(TCID_path)
-            print("Testcase ID: ", testcase_id)
+                robo_path_des = TCID_path + "/test.robot"
+                shutil.copy(robo_path, robo_path_des)
 
-            robo_path_des = TCID_path + "/test.robot"
-            shutil.copy(robo_path, robo_path_des)
+                vlc_path = testcase_content.find('VLC_Path').text.replace("\\", "/")
+                vlc_sou = vlc_itc_path + vlc_path
+                vlc_name = os.path.basename(vlc_sou)
+                vlc_des = TCID_path + "/" + vlc_name
+                shutil.copy(vlc_sou, vlc_des)
 
-            vlc_path = testcase_content.find('VLC_Path').text.replace("\\", "/")
-            vlc_sou = vlc_itc_path + vlc_path
-            vlc_name = os.path.basename(vlc_sou)
-            vlc_des = TCID_path + "/" + vlc_name
-            shutil.copy(vlc_sou, vlc_des)
+                itc_path = testcase_content.find('Img_Path').text.replace("\\", "/")
+                itc_sou = vlc_itc_path + itc_path
+                itc_name = os.path.basename(itc_sou).replace(".itc", "")
+                itc_file = ET.parse(itc_sou)
+                read_itc_file = itc_file.getroot()
+                print("Path of itc file", itc_sou)
 
-            itc_path = testcase_content.find('Img_Path').text.replace("\\", "/")
-            itc_sou = vlc_itc_path + itc_path
-            itc_name = os.path.basename(itc_sou).replace(".itc", "")
-            itc_file = ET.parse(itc_sou)
-            read_itc_file = itc_file.getroot()
-            print("Path of itc file", itc_sou)
-
-            csv_path = TCID_path + "/" + itc_name + "_set.csv"
-            csv = open(csv_path, "w")
-            csv_data = "No,Image,Content\n"
-            csv.write(csv_data)
-
-            for itc_symbology_content in read_itc_file:
-                image_path = itc_symbology_content.text.strip()
-                image_path = image_path.replace("\\", "/")
-                image_sou = image_origin_path + image_path
-                image_name = os.path.basename(image_sou)
-                image_des = TCID_path + "/" + image_name
-                shutil.copy(image_sou, image_des)
-                print("Copy image ", image_name, "success")
-
-                continue_flag = itc_symbology_content.find("expected_data")
-                if continue_flag is None:
-                    continue
-
-                expected_data = itc_symbology_content.find("expected_data").text
-                if expected_data is None:
-                    expected_data = '""'
-                else:
-                    expected_data = '"' + expected_data + '"'
-                csv_data = '0,' + image_name + ',""' + expected_data + '"\n'
+                csv_path = TCID_path + "/" + itc_name + "_set.csv"
+                csv = open(csv_path, "w")
+                csv_data = "No,Image,Content\n"
                 csv.write(csv_data)
-                print("Write data ", csv_data, " success ")
 
-            csv.close()
+                for itc_symbology_content in read_itc_file:
+                    image_path = itc_symbology_content.text.strip()
+                    image_path = image_path.replace("\\", "/")
+                    image_sou = image_origin_path + image_path
+                    image_name = os.path.basename(image_sou)
+                    image_des = TCID_path + "/" + image_name
+                    shutil.copy(image_sou, image_des)
+                    print("Copy image ", image_name, "success")
+
+                    continue_flag = itc_symbology_content.find("expected_data")
+                    if continue_flag is None:
+                        continue
+
+                    expected_data = itc_symbology_content.find("expected_data").text
+                    if expected_data is None:
+                        expected_data = '""'
+                    else:
+                        expected_data = '"' + expected_data + '"'
+                    csv_data = '0,' + image_name + ',""' + expected_data + '"\n'
+                    csv.write(csv_data)
+                    print("Write data ", csv_data, " success ")
+
+                csv.close()
+
+###GUI###
+window = tk.Tk()
+window.geometry("500x300")
+
+file_load_button = tk.Button(window, text="Load File", command=lambda: choosefile_insertlabel(file_load_path, "Choose load file", file_load_label))
+file_load_button.grid(row=0, column=0)
+file_load_label = tk.Label(window, text=file_load_path, fg="blue")
+file_load_label.grid(row=0, column=1)
+
+file_robot_button = tk.Button(window, text="test.robot File", command=lambda: choosefile_insertlabel(robo_path, "Choose test.robot file", file_robot_label))
+file_robot_button.grid(row=1, column=0)
+file_robot_label = tk.Label(window, text=robo_path, fg="blue")
+file_robot_label.grid(row=1, column=1)
+
+folder_testresult_button = tk.Button(window, text="TestResult Folder", command=lambda: choosefolder_insertlabel(test_result_path, "Choose TestResult folder", folder_testresult_label))
+folder_testresult_button.grid(row=2, column=0)
+folder_testresult_label = tk.Label(window, text=test_result_path, fg="blue")
+folder_testresult_label.grid(row=2, column=1)
+
+folder_config_button = tk.Button(window, text="Config Folder", command=lambda: choosefolder_insertlabel(vlc_itc_path, "Choose config folder", folder_config_label))
+folder_config_button.grid(row=3, column=0)
+folder_config_label = tk.Label(window, text=vlc_itc_path, fg="blue")
+folder_config_label.grid(row=3, column=1)
+
+folder_originimage_button = tk.Button(window, text="Origin Image Folder", command=lambda: choosefolder_insertlabel(image_origin_path, "Choose origin image folder", folder_originimage_label))
+folder_originimage_button.grid(row=4, column=0)
+folder_originimage_label = tk.Label(window, text=image_origin_path, fg="blue")
+folder_originimage_label.grid(row=4, column=1)
+
+OK_button = tk.Button(window, text="OK", command=lambda: run(file_load_label.cget("text"), file_robot_label.cget("text"), folder_testresult_label.cget("text"), folder_config_label.cget("text"), folder_originimage_label.cget("text")))
+OK_button.grid(row=5, column=0)
+
+window.mainloop()
